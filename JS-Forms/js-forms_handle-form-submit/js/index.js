6a0e6e8f7d4f75af90f418b2ee66845b900ec48c
Sham <shamrdowaji@gmail.com>
2025-03-12 16:52:28 +0100

    solved

--- JS-Forms/js-forms_handle-form-submit/js/index.js
+++ JS-Forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,19 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formElements = event.target.elements;
+  const firstName = formElements.firstName.value;
+  console.log("WHAT S YOUR NAME???", firstName);
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+
+  event.target.reset();
+  formElements.firstName.focus();
+});
