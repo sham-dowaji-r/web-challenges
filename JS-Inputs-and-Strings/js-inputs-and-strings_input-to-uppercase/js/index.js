bdb7d22d65e938c716df4eaba200e880e63bc6e5
Sham <shamrdowaji@gmail.com>
2025-03-12 11:42:54 +0100

    uppercase solved

--- JS-Inputs-and-Strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ JS-Inputs-and-Strings/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,10 @@
 console.clear();
+
+const firstInput = document.querySelector('[data-js = "first-input"]');
+const buttonUppercase = document.querySelector(
+  '[data-js = "button-uppercase"]'
+);
+
+buttonUppercase.addEventListener("click", () => {
+  firstInput.value = firstInput.value.toUpperCase();
+});
