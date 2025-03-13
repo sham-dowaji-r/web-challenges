c32db1ed1b31b7fe6cbe0f7b8177d1bf67964c86
Sham <shamrdowaji@gmail.com>
2025-03-13 11:44:28 +0100

    challenge solved

--- JS-Createelement/js-createelement_toast-messages/js/index.js
+++ JS-Createelement/js-createelement_toast-messages/js/index.js
@@ -4,10 +4,16 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener("click", (event) => {
   // Exercise: Append a new entry to the toast messages container
+
+  const toastMessage = document.createElement("li");
+  toastMessage.textContent = "New toast message!";
+  toastMessage.classList.add("toast-container__message");
+  toastContainer.append(toastMessage);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.textContent = "";
 });
