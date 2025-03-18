a84dc832ac7f8f3a44ed8c2c71ba4df6d0d721db
Sham <shamrdowaji@gmail.com>
2025-03-18 15:55:39 +0100

    solvd

--- JS-Array-Methods/js-array-methods_filter/js/index.js
+++ JS-Array-Methods/js-array-methods_filter/js/index.js
@@ -24,13 +24,21 @@ const cards = [
   },
 ];
 
-const onlyCardWithIdTwo = null;
+const onlyCardWithIdTwo = cards.filter((card) => card.id === "2");
+console.log(onlyCardWithIdTwo);
 
-const allCardsWith3Tags = null;
+const allCardsWith3Tags = cards.filter((card) => card.tags.length === 3);
+console.log(allCardsWith3Tags);
 
-const allCardsThatAreNotBookmarked = null;
+const allCardsThatAreNotBookmarked = cards.filter((card) => !card.isBookmarked);
+console.log(allCardsThatAreNotBookmarked);
 
-const allCardsWithTagsHTMLOrJSThatAreBookmarked = null;
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards.filter(
+  (card) =>
+    card.isBookmarked &&
+    (card.tags.includes("js") || card.tags.includes("html"))
+);
+console.log(allCardsWithTagsHTMLOrJSThatAreBookmarked);
 
 export {
   onlyCardWithIdTwo,
