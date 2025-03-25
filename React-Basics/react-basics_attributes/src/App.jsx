a3bbdd9f4534017eb4dd608f6ae2d1442c28cf17
Sham <shamrdowaji@gmail.com>
2025-03-25 17:12:26 +0100

    solved

--- React-Basics/react-basics_attributes/src/App.jsx
+++ React-Basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,21 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return <Article />;
+}
+
+export function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">IT IS WIKIPEDIA</h2>
+      <label htmlFor="input">Search</label>
+      <input name="input" id="input"></input>
+      <a
+        className="article__link"
+        href="https://de.wikipedia.org/wiki/Wikipedia:Hauptseite"
+      >
+        Click Here To Go To Wikibedia
+      </a>
+    </article>
+  );
 }
