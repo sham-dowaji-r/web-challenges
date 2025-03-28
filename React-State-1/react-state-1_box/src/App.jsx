20a579d3cbb9fd39eacb74ea2c144d9b7e12316c
Sham <shamrdowaji@gmail.com>
2025-03-28 11:00:53 +0100

    solved

--- React-State-1/react-state-1_box/src/App.jsx
+++ React-State-1/react-state-1_box/src/App.jsx
@@ -1,18 +1,24 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let isActive = false;
+  // let isActive = false;
+  //We should replace the `let` variable with the `useState` function and Set the initial state value to `false`.
+  const [isActive, setIsActive] = useState(false);
 
   function handleClick() {
-    isActive = !isActive;
-    // Check that the value changes correctly.
-    console.log(isActive);
+    //isActive = !isActive;
+    //We should use the `set` function from `useState` to update the value of the `isActive` state.
+
+    setIsActive(!isActive);
   }
 
   return (
     <main>
       <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
+      <button onClick={handleClick}>
+        {isActive ? "Deactivate" : "Activate"}
+      </button>
     </main>
   );
 }
