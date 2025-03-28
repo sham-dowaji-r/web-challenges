0fc4dd77a6e5996fc73bc9fd89d75688476d681d
Sham <shamrdowaji@gmail.com>
2025-03-28 11:23:54 +0100

    solved

--- React-State-1/react-state-1_counter/src/App.jsx
+++ React-State-1/react-state-1_counter/src/App.jsx
@@ -1,26 +1,22 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let count = 0;
-
+  const [count, setCount] = useState(0);
+  function handleClickIncrement() {
+    setCount(count + 1);
+  }
+  function handleClickDecrement() {
+    setCount(count - 1);
+  }
   return (
     <div className="container">
       <h1>{count}</h1>
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={handleClickDecrement}>
           -
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={handleClickIncrement}>
           +
         </button>
       </div>
