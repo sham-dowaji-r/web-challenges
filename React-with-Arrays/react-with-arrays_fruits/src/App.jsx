28bdc5c93d37d30a35ccde6a77ed746b458c60ae
Sham <shamrdowaji@gmail.com>
2025-03-28 15:52:40 +0100

    solvd

--- React-with-Arrays/react-with-arrays_fruits/src/App.jsx
+++ React-with-Arrays/react-with-arrays_fruits/src/App.jsx
@@ -1,11 +1,19 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    { id: 1, name: "🍌 Banana", color: "yellow" },
+    { id: 2, name: "🍎 Apple", color: "red" },
+    { id: 3, name: "🍊 Orange", color: "orange" },
+    { id: 4, name: "🍉 Watermelon", color: "green" },
+    { id: 5, name: "🍇 Grapes", color: "purple" },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card key={fruit.id} text={fruit.name} color={fruit.color} />
+      ))}
     </div>
   );
 }
--- React-with-Arrays/react-with-arrays_fruits/src/components/Card.jsx
+++ React-with-Arrays/react-with-arrays_fruits/src/components/Card.jsx
@@ -1,5 +1,9 @@
 import "./Card.css";
 
-export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+export default function Card({ text, color }) {
+  return (
+    <p className="card" style={{ background: color }}>
+      {text}
+    </p>
+  );
 }
