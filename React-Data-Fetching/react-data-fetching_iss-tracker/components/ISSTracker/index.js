bc508a99cf9d5b877c35bb76ce8455192c4a81d6
Sham <shamrdowaji@gmail.com>
2025-04-14 11:19:10 +0200

    data fetching

--- React-Data-Fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ React-Data-Fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -1,16 +1,32 @@
-import { useEffect, useState } from "react";
+import useSWR from "swr";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+  return response.json();
+};
 
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
+  const { data, error, isLoading, mutate } = useSWR(URL, fetcher, {
+    refreshInterval: 4000,
+  });
+  if (isLoading) return <p>Loading ISS position</p>;
+  if (error) return <p>Faild to load ISS position:{error.message}</p>;
+  if (!data) return null;
+  /*const [coords, setCoords] = useState({
     longitude: 0,
     latitude: 0,
   });
-
-  async function getISSCoords() {
+*/
+  /* async function getISSCoords() {
     try {
       const response = await fetch(URL);
       if (response.ok) {
@@ -21,8 +37,8 @@ export default function ISSTracker() {
       console.error(error);
     }
   }
-
-  useEffect(() => {
+*/
+  /*useEffect(() => {
     const timer = setInterval(() => {
       getISSCoords();
     }, 5000);
@@ -31,14 +47,14 @@ export default function ISSTracker() {
       clearInterval(timer);
     };
   }, []);
-
+*/
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Map longitude={data.longitude} latitude={data.latitude} />
       <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        longitude={data.longitude}
+        latitude={data.latitude}
+        onRefresh={() => mutate()}
       />
     </main>
   );
